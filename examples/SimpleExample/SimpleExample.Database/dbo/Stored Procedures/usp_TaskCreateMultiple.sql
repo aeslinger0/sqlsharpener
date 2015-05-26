7b009fdd219018ae68769a07c120a976a37c8185
aeslinger0 <adam.eslinger@gmail.com>
2015-05-26 12:32:35 -0500

    Added Entity Framework Code-First example

---
+++ examples/SimpleExample/SimpleExample.Database/dbo/Stored Procedures/usp_TaskCreateMultiple.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[usp_TaskCreateMultiple]
+	@tasks Tasks READONLY
+AS
+	DECLARE @ids TABLE(id int)
+
+	INSERT INTO Tasks
+		(Name, [Description], TaskStatusId, Created, CreatedBy, Updated, UpdatedBy)
+	OUTPUT inserted.Id into @ids
+	SELECT
+		Name, [Description], TaskStatusId, Created, CreatedBy, Updated, UpdatedBy
+	FROM @tasks
+
+
+	SELECT id from @ids
---
+++ examples/SimpleExample/SimpleExample.Database/dbo/Types/Tasks.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE Tasks as TABLE(
+	Name varchar(50),
+	[Description] varchar(1000),
+	TaskStatusId int,
+	Created datetime,
+	CreatedBy varchar(50),
+	Updated datetime,
+	UpdatedBy varchar(50)
+)
